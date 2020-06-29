c5ccb7c0d776dd34d613de8fb7ed336a64c23525
naim833 <naim.bhunnoo@gmail.co>
2020-06-29 11:15:13 +0400

    version 0.2

--- plus_moins.c
+++ plus_moins.c
@@ -33,15 +33,77 @@ int main()
 {
         int lower=0, upper=100, count=1;
 	int choix_joueur;
-        int a=0;
+        int a=0,x=0;
+        int diff;
         srand(time(0));
         int valeur_a_trouver=randoms(lower,upper,count);
-        while (choix_joueur != valeur_a_trouver)
+        printf("Veuillez choisir votre mode de difficulte : \n");
+        printf("1-facile\n");
+        printf("2-normale\n");
+        printf("3-difficile\n");
+        scanf("%d",&diff);
+        while((diff>3)||(diff<1))
+        {   switch(diff)
+            { case 1:printf("Vous etes en mode facile");
+                     break;
+                     
+              case 2:printf("Vous etes en mode normale");
+                     break;
+                    
+              case 3:printf("Vous etes en mode difficile");
+                     break;
+                    
+              default:printf("Veuillez choisir un chiffre valide.\n"); 
+                      printf("Veuillez choisir votre mode de difficulte : \n");
+                      scanf("%d",&diff);
+                      
+            } 
+        }             
+       
+       if (diff==2)
         {
+          for(int i=1;i<=25;i++)
+          {
             printf("Veuillez entrer un chiffre entier : \n");
             scanf("%d",&choix_joueur);
             plus_moins(choix_joueur,valeur_a_trouver);
+            if (choix_joueur == valeur_a_trouver)
+             {
+               break;
+             }
             a++;
+          }
+            
+        }
+       else
+        {
+          if (diff==3)
+          {
+            for(int i=1;i<=10;i++)
+            {
+               printf("Veuillez entrer un chiffre entier : \n");
+               scanf("%d",&choix_joueur);
+               plus_moins(choix_joueur,valeur_a_trouver);
+                if (choix_joueur == valeur_a_trouver)
+                 {
+                    break;
+                 }
+               a++;
+            }
+           
+            
+          }
+          else
+          {
+            while (choix_joueur != valeur_a_trouver)
+            {
+               printf("Veuillez entrer un chiffre entier : \n");
+               scanf("%d",&choix_joueur);
+               plus_moins(choix_joueur,valeur_a_trouver);
+               a++;
+            }
+           
+          }
         }
         printf("Nombre de tentatives : %d\n",a);
 }
